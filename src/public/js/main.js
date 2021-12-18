96c82ed754088e018917e3a8575467019902f0c5
acye <edwinalejandro3016@gmail.com>
2021-12-18 11:59:59 -0400

    algunos detalles

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -19,18 +19,18 @@
 
           // animation section
 
-    self.addEventListener('scroll',()=>{
-        let value = window.scrollY;
-        text.style.top = 50 + value * -0.5 + '%';
-        bird1.style.top = value * -1.5 + 'px';
-        bird1.style.left = value * 2 + 'px';
-        bird2.style.top = value * -1.5 + 'px';
-        bird2.style.left = value * -5 + 'px';
-        btn.style.marginTop = value * 1.5 + 'px';
-        rocks.style.top = value * -0.12 + 'px';
-        forest.style.top = value * 0.25 + 'px';
-        header.style.top = value * 0.5 + 'px'; 
-    })
+    // self.addEventListener('scroll',()=>{
+    //     let value = window.scrollY;
+    //     text.style.top = 50 + value * -0.5 + '%';
+    //     bird1.style.top = value * -1.5 + 'px';
+    //     bird1.style.left = value * 2 + 'px';
+    //     bird2.style.top = value * -1.5 + 'px';
+    //     bird2.style.left = value * -5 + 'px';
+    //     btn.style.marginTop = value * 1.5 + 'px';
+    //     rocks.style.top = value * -0.12 + 'px';
+    //     forest.style.top = value * 0.25 + 'px';
+    //     header.style.top = value * 0.5 + 'px'; 
+    // })
 
 
 
