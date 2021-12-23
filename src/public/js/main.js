7a9eb1e9411234c946f912d35ae258550f3e3393
acye <edwinalejandro3016@gmail.com>
2021-12-23 07:47:29 -0400

    another

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,16 +1,22 @@
-    let text = document.getElementById('text'); 
-    let bird1 = document.getElementById('bird1'); 
-    let bird2 = document.getElementById('bird2'); 
-    let btn = document.getElementById('btn'); 
-    let rocks = document.getElementById('rocks');
-    let water = document.getElementById('water');
+
     let header = document.getElementById('header');
     const navigation = document.querySelector('.navigation');
+    const itemsNavResp = document.querySelectorAll('.items-nav a');
 
+    //items nav clases active
+    const itemsNav = document.querySelectorAll('.navigation a');
+    itemsNav.forEach((item)=>{
+      item.addEventListener('click',(e)=>{
+        itemsNav.forEach((item)=>{
+          item.classList.remove('activo');
+        })
+        e.currentTarget.classList.add('activo');
+      })
+    })
     //carousel
-    const carousel = document.getElementById('carousel');
-    const btnLeft = document.getElementById('btn-left');
-    const btnRight = document.getElementById('btn-right');
+    // const carousel = document.getElementById('carousel');
+    const carousel = document.querySelector('.carousel');
+    const tituloMinisterio = document.getElementById('ministerio');
 
     //bars
     const bars = document.getElementById('bars');
@@ -20,6 +26,24 @@
     const btnOracion = document.getElementById('btn-oracion');
     const motivoOracion = document.querySelector('.motivo-oracion');
 
+      //animation scroll
+
+      $(document).ready(function(){
+        function scroll(event) {
+            if (this.hash !== "") {
+              event.preventDefault();
+              var hash = this.hash;
+              $('html, body').animate({
+                scrollTop: $(hash).offset().top
+              }, 500, function(){
+                window.location.hash = hash;
+              });
+            } // End if
+  
+          };// end function
+        $("a").on('click', scroll);
+
+      });
 
     window.addEventListener('scroll',()=>{
         header.classList.toggle('sticky', window.scrollY > 0);
@@ -34,13 +58,18 @@
         if( navbar.classList.contains('activo')){
           bars.style.color = '#fff';
           bars.classList.replace('fa-bars','fa-times'); 
-          body.style.overflowY = "hidden";
-
+          itemsNavResp.forEach(item=>{
+            item.addEventListener('click',(e)=>{
+               bars.style.color = "rgba(9, 75, 101,.8)";
+               bars.classList.replace('fa-times','fa-bars');
+               navbar.classList.remove('activo');
+            })
+          })
         }else{
           bars.style.color = "rgba(9, 75, 101,.8)";
           bars.classList.replace('fa-times','fa-bars')
           ;  
-          body.style.overflowY = "visible";
+          // body.style.overflowY = "visible";
         }
     });
 
@@ -67,6 +96,20 @@
       }
   });
     //carousel
+    carousel.addEventListener('mouseover',(e)=>{
+      if(e.target.dataset.ministerio === undefined){
+        return false;
+      }else{
+        let parrafo = e.target.dataset.ministerio;
+        tituloMinisterio.textContent = parrafo;
+      }
+    });
+
+    carousel.addEventListener('mouseleave',(e)=>{
+      tituloMinisterio.textContent = 'Ministerios';
+    });
+
+
     window.addEventListener('load',function(){
         new Glider(document.querySelector('.glider'),{
             slidesToShow: 1,
@@ -77,14 +120,7 @@
               next: '.glider-next'
             },responsive: [
                 {
-                    breakpoint: 350, 
-                    settings: {
-                      slidesToShow: 1,
-                      slidesToScroll: 1,
-                    }
-                  },
-                {
-                  breakpoint: 700, 
+                  breakpoint: 450, 
                   settings: {
                     slidesToShow: 2,
                     slidesToScroll: 2,
@@ -100,22 +136,4 @@
         });
     })
 
-    //animation scroll
-
-    $(document).ready(function(){
-        function scroll(event) {
-            if (this.hash !== "") {
-              event.preventDefault();
-              var hash = this.hash;
-              $('html, body').animate({
-                scrollTop: $(hash).offset().top
-              }, 800, function(){
-                window.location.hash = hash;
-              });
-            } // End if
   
-          };// end function
-        $("a").on('click', scroll);
-
-      });
-
